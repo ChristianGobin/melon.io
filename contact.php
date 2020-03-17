855299466978641568fa73427ada03d696f4c2ea
Christian Gobin <chrisgobin94@gmail.com>
2020-03-17 00:02:07 -0400

    server side validation php
    Check all fields using php.

--- contact.php
+++ contact.php
@@ -1,50 +1,92 @@
+<?php
+//server side validation
+$error = '';
+if ($_POST) {
+    if (!$_POST["email"]) {
+        $error .= 'Email not submitted by user. <br>';
+        echo $error;
+    }
+    if (!$_POST["subject"]) {
+        $error .= 'Subject not submitted by user. <br>';
+        echo $error;
+    }
+    if (!$_POST["message"]) {
+        $error .= 'Message not submitted by user. <br>';
+        echo $error;
+    }
+    if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL === false)) {
+        $error .= "Invalid email format";
+        echo $error . "<br>";
+    }
+    if ($error != '') {
+        echo "<div class='alert alert-danger' role='alert'>" . $error . "</div>";
+    } else {
+        $emailTo = "chrisgobin94@gmail.com";
+        $subject = $_POST["subject"];
+        $from = $_POST["email"];
+        $message = $_POST["message"];
+        $successMsg = "<div class='alert alert-success' role='alert'> Sent! </div>";
+    }
+}
+?>
 <!DOCTYPE html>
 
 <head>
     <title>Melon Contact Form</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
-
 </head>
 
 <body>
     <div class="container">
         <h5 class="display-4">Contact</h5>
-        <div id="error"></div>
-        <form id='contact-form'>
+        <div id="error"><? echo $error ?></div>
+        <form id='contact-form' method="post">
             <div class="form-group">
                 <label for="email">Email address</label>
-                <input type="email" class="form-control" id="email" placeholder="Enter email">
+                <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
             <div class="form-group">
                 <label for="subject">Subject</label>
-                <input type="text" class="form-control" id="subject">
+                <input type="text" class="form-control" id="subject" name="subject">
             </div>
             <div class="form-group">
                 <label for="message">Message</label>
-                <textarea id="message" class="form-control" row="3"></textarea>
+                <textarea id="message" class="form-control" row="3" name="message"></textarea>
             </div>
             <button type="submit" id="submit" class="btn btn-primary">Submit</button>
         </form>
+        <ul>
+            <?php
+            foreach ($_POST as $item) {
+                echo "<li>" . $item . "</li>";
+            }
+            ?>
+        </ul>
     </div>
 
     <!--JQuery, Bootstrap4 Js -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script type='text/javascript'>
+        //Check if fields are filled. Hookup on Submit event handler. 
+        //Client side validation.
+
         $("#contact-form").submit((e) => {
             let errorMsg = "";
             e.preventDefault();
             if ($("#email").val() == "") {
-                errorMsg += "Email cannot be blank.";
+                errorMsg += "<div class='alert alert-danger' role='alert'> Email cannot be blank. </div>";
                 $("#error").html(errorMsg);
             }
             if ($("#subject").val() == "") {
-                errorMsg += "Subject cannot be blank.";
+                errorMsg += "<div class='alert alert-danger' role='alert'> Subject cannot be blank. </div>";
                 $("#error").html(errorMsg);
             }
             if ($("#message").val() == "") {
-                errorMsg += "Message cannot be blank.";
+                errorMsg += "<div class='alert alert-danger' role='alert'> Message cannot be blank. </div>";
                 $("#error").html(errorMsg);
+            } else {
+                $("#contact-form").unbind('submit').submit();
             }
         })
     </script>
--- home.php
+++ home.php
@@ -1,175 +1,173 @@
-  <!DOCTYPE html>
-  <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
-  <head>
-      <?php
-        define("TITLE", "Melon.io")
-        ?>
-      <!-- Required meta tags -->
-      <meta charset="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-      <!--My css-->
-      <link rel="stylesheet" href="./style.css" />
-      <!-- Bootstrap CSS -->
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+<head>
+    <?php
+    define("TITLE", "Melon.io")
+    ?>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!--My css-->
+    <link rel="stylesheet" href="./style.css" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
 
-      <title><?php echo TITLE ?></title>
-  </head>
+    <title><?php echo TITLE ?></title>
+</head>
 
-  <body>
-      <?php
-        ?>
-      <div class="container">
-          <!--Responsive Navbar-->
-          <nav class="navbar navbar-expand-lg navbar-light bg-light">
-              <a class="navbar-brand" href="#">Melon</a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul class="navbar-nav mr-auto">
-                      <li class="nav-item active">
-                          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                      </li>
-                      <li class="nav-item">
-                          <a class="nav-link" href="#">Link</a>
-                      </li>
-                      <li class="nav-item dropdown">
-                          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                              Dropdown
-                          </a>
-                          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                              <a class="dropdown-item" href="#">Action</a>
-                              <a class="dropdown-item" href="#">Another action</a>
-                              <div class="dropdown-divider"></div>
-                              <a class="dropdown-item" href="#">Something else here</a>
-                          </div>
-                      </li>
-                  </ul>
-                  <form class="form-inline my-2 my-lg-0">
-                      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-                          Search
-                      </button>
-                  </form>
-              </div>
-          </nav>
-          </br>
-          <!-- Flex Box Login + Brief Intro-->
-          <div class="d-flex flex-column flex-md-row align-items-center border-top">
-              <!--Jumbotron Brief-->
-              <div class="jumbotron p-2">
-                  <h1 class="display-4">Melon</h1>
-                  <p class="lead">
-                      Melon is a platform for people to view the current market landscape of Farming throughout the world.
-                  </p>
-                  <hr class="my-4" />
-                  <p>
-                      Built with Bootstrap4 and Flexbox.
-                  </p>
-                  <p class="lead">
-                      <a class="btn btn-primary btn-lg" href="#" role="button">Get Started</a>
-                  </p>
-              </div>
-              <!-- Login form-->
-              <div class="card card-signin p-2">
-                  <div class="card-body">
-                      <h5 class="card-title text-center">Sign In</h5>
-                      <form class="form-signin">
-                          <div class="form-label-group">
-                              <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus />
-                              <label for="inputEmail"></label>
-                          </div>
+<body>
+    <div class="container">
+        <!--Responsive Navbar-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Melon</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+                        Search
+                    </button>
+                </form>
+            </div>
+        </nav>
+        </br>
+        <!-- Flex Box Login + Brief Intro-->
+        <div class="d-flex flex-column flex-md-row align-items-center border-top">
+            <!--Jumbotron Brief-->
+            <div class="jumbotron p-2">
+                <h1 class="display-4">Melon</h1>
+                <p class="lead">
+                    Melon is a platform for people to view the current market landscape of Farming throughout the world.
+                </p>
+                <hr class="my-4" />
+                <p>
+                    Built with Bootstrap4 and Flexbox.
+                </p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="#" role="button">Get Started</a>
+                </p>
+            </div>
+            <!-- Login form-->
+            <div class="card card-signin p-2">
+                <div class="card-body">
+                    <h5 class="card-title text-center">Sign In</h5>
+                    <form class="form-signin">
+                        <div class="form-label-group">
+                            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus />
+                            <label for="inputEmail"></label>
+                        </div>
 
-                          <div class="form-label-group">
-                              <input type="password" id="inputPassword" class="form-control" placeholder="Password" required />
-                              <label for="inputPassword"></label>
-                          </div>
+                        <div class="form-label-group">
+                            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required />
+                            <label for="inputPassword"></label>
+                        </div>
 
-                          <div class="custom-control custom-checkbox">
-                              <input type="checkbox" class="custom-control-input" id="customCheck1" />
-                              <label class="custom-control-label" for="customCheck1">Remember password</label>
-                          </div>
-                          <br />
-                          <button class="btn btn-lg btn-success btn-block " type="submit">
-                              Sign in
-                          </button>
-                          <hr class="my-4" />
-                      </form>
-                  </div>
-              </div>
-          </div>
-          <br />
-          <!--Flex Column-->
-          <div class="d-flex flex-column">
-              <!--additional features-->
-              <div class="card p-2">
-                  <div class="card-header">
-                      Featured
-                  </div>
-                  <div class="card-body">
-                      <h5 class="card-title">Top Fruit Yields of 2020</h5>
-                      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                      <a href="#" class="btn btn-primary">More</a>
-                  </div>
-              </div>
-              <br />
-              <!--main content with sidebar-->
-              <div class="d-flex flex-column flex-md-row align-items-center justify-content-around">
-                  <div class="card p-2" style="width: 18rem;">
-                      <img class="card-img-top" src=".\images\philip-junior-mail-BpUkWK6hfJA-unsplash.jpg" alt="Card image cap">
-                      <div class="card-body">
-                          <h5 class="card-title">Why Altitude Isn't Everything</h5>
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                          <a href="#" class="btn btn-primary">Go somewhere</a>
-                      </div>
-                  </div>
-                  <div class="card p-2" style="width: 18rem;">
-                      <img class="card-img-top" src=".\images\peter-hammer-ggQy5lGtwb0-unsplash.jpg" alt="Card image cap">
-                      <div class="card-body">
-                          <h5 class="card-title">South American Earners</h5>
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                          <a href="#" class="btn btn-primary">Go somewhere</a>
-                      </div>
-                  </div>
-                  <div class="card p-2" style="width: 18rem;">
-                      <img class="card-img-top" src=".\images\henry-be-JR8XZ-gdZrA-unsplash.jpg" alt="Card image cap">
-                      <div class="card-body">
-                          <h5 class="card-title">Bitter Blends</h5>
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                          <a href="#" class="btn btn-primary">Go somewhere</a>
-                      </div>
-                  </div>
-              </div>
-              <!-- Footer -->
-              <footer class="page-footer font-small blue pt-4">
-                  <!-- Footer Links -->
-                  <div class="container-fluid text-center text-md-left">
-                      <!-- Grid row -->
-                      <div class="row">
-                          <hr class="clearfix w-100 d-md-none pb-3">
-                          <!-- Grid column -->
-                          <div class="col-md-3 mb-md-0 mb-3">
-                              <!-- Links -->
-                              <h5 class="text-uppercase">Links</h5>
-                              <ul class="list-unstyled">
-                                  <li>
-                                      <a href="./contact.php">Contact Us</a>
-                                  </li>
-                                  <li>
-                                      <a href="github.com/christiangobin">My Github</a>
-                                  </li>
-                              </ul>
-                          </div>
-                      </div>
-                  </div>
-              </footer>
-          </div>
-          <!-- Optional JavaScript -->
-          <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-          <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="customCheck1" />
+                            <label class="custom-control-label" for="customCheck1">Remember password</label>
+                        </div>
+                        <br />
+                        <button class="btn btn-lg btn-success btn-block " type="submit">
+                            Sign in
+                        </button>
+                        <hr class="my-4" />
+                    </form>
+                </div>
+            </div>
+        </div>
+        <br />
+        <!--Flex Column-->
+        <div class="d-flex flex-column">
+            <!--additional features-->
+            <div class="card p-2">
+                <div class="card-header">
+                    Featured
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Top Fruit Yields of 2020</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">More</a>
+                </div>
+            </div>
+            <br />
+            <!--main content with sidebar-->
+            <div class="d-flex flex-column flex-md-row align-items-center justify-content-around">
+                <div class="card p-2" style="width: 18rem;">
+                    <img class="card-img-top" src=".\images\philip-junior-mail-BpUkWK6hfJA-unsplash.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Why Altitude Isn't Everything</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card p-2" style="width: 18rem;">
+                    <img class="card-img-top" src=".\images\peter-hammer-ggQy5lGtwb0-unsplash.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">South American Earners</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card p-2" style="width: 18rem;">
+                    <img class="card-img-top" src=".\images\henry-be-JR8XZ-gdZrA-unsplash.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Bitter Blends</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <!-- Footer -->
+            <footer class="page-footer font-small blue pt-4">
+                <!-- Footer Links -->
+                <div class="container-fluid text-center text-md-left">
+                    <!-- Grid row -->
+                    <div class="row">
+                        <hr class="clearfix w-100 d-md-none pb-3">
+                        <!-- Grid column -->
+                        <div class="col-md-3 mb-md-0 mb-3">
+                            <!-- Links -->
+                            <h5 class="text-uppercase">Links</h5>
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a href="./contact.php">Contact Us</a>
+                                </li>
+                                <li>
+                                    <a href="github.com/christiangobin">My Github</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
 
-  </html>
+</html>
